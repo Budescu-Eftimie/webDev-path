7ca84360d8c21e2cebd5ea01a3dc60d3838a4f7d
Budescu Eftimie <budescueftimie@gmail.com>
2022-02-06 05:24:52 +0100

    add more array destructuring example

--- Javascript/destructuring.js
+++ Javascript/destructuring.js
@@ -5,7 +5,7 @@
 
 //-----Array destructuring example-----//
 
-const myArray = ["Timi", "John", "Mathew", "Tony"];
+const myArray = ["Timi", "John", "Mathew", "Tony", "Brad", "Jenny", "Andreea"];
 
 /*  from the above [myArray] if we need to store both of the first 
     two persons in they're own variable we could do it like this
@@ -18,5 +18,15 @@ console.log(pers1, pers2); // logs Timi John
 //  but with destructuring is much better
 
 const [persTwo, persThree] = myArray;
-
 console.log(persTwo, persThree); // also logs Timi John
+
+
+/*  in the below example we single out the first two names of [myArray]
+    while the rest of the names are stored in a new list [otherPersons]
+    using javaScript spread operator*/
+
+const [timi, john, ...otherPersons] = myArray;
+
+console.log(timi, john);
+console.log(otherPersons);
+console.log(myArray);
